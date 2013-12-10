b574f6e1a2c2d1b70fdb626d14299984561859a6
Ji Rujun <jirujun@gmail.com>
2013-12-10 13:20:49 +0800

    Task H: Sending Mail and Integration Testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+	
+	# Send mail conifg 
+	config.action_mailer.delivery_method = :test
 end
