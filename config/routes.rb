7672df1163ecd4e3d2ae529a9819482db6674a3c
Ji Rujun <jirujun@gmail.com>
2013-12-29 10:18:34 +0800

    Task I:Loging In I4 OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Depot::Application.routes.draw do
-  get "admin/index"
+  get 'admin' => 'admin#index'
 
-  get "sessions/new"
+	controller :sessions do
+		get 'login' => :new
+		post 'login' => :create
+		delete 'logout' => :destroy
+	end
 
-  get "sessions/create"
 
-  get "sessions/destroy"
 
   resources :users
 
