585ea6d1b974795924252673383c0f4071dac3fd
Ji Rujun <jirujun@gmail.com>
2014-01-07 07:18:53 +0800

    initializers

--- config/routes.rb
+++ config/routes.rb
@@ -8,28 +8,17 @@ Depot::Application.routes.draw do
 	end
 
 
-
+scope '(:locale)' do
   resources :users
-
-
   resources :orders
-
-
   resources :line_items
-
-
   resources :carts
-
-
-  get "store/index"
-
   resources :products do
 		get :who_bought, on: :member
 	end
 
-
   root to: 'store#index', as: 'store'
-
+end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
